fd4c39c028007b11d98f39f60a7b44406eae6a3c
Khanh Le <khanhle@unomaha.edu>
2023-01-30 16:19:11 -0600

    Set up External API call and filter out with frequency to determine what days to get prices

---
+++ Controllers/FmpController.cs
@@ -0,0 +1,69 @@
+﻿using KFinance.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KFinance.Controllers
+{
+    [ApiController]
+    [Route("api/{controller}")]
+    public class FmpController : Controller
+    {
+        private readonly HttpClient client = new()
+        {
+            BaseAddress = new Uri("https://financialmodelingprep.com/api/v3"),
+        };
+        private readonly IConfiguration _config;
+        public FmpController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet()]
+        public async Task<string> GetPrice(RequestDTO requestDTO)
+        {
+            try
+            {
+                string apikey = this._config["FMP:Apikey"]!;
+                string parameters = $"?apikey={apikey}";
+
+                string url = client.BaseAddress!.ToString() + $"/historical-price-full/{requestDTO.TickerSymbol}";
+                client.BaseAddress = new Uri(url);
+
+                using var temp = await client.GetAsync(parameters);
+                var a = await temp.Content.ReadAsStringAsync();
+
+                var res = await temp.Content.ReadFromJsonAsync<ResponseFromAPI>();
+                string result = string.Empty;
+                if (res is not null)
+                {
+                    var filteredHistory = res.Historical.ToList().Where(h => DateTime.Compare(h.Date, requestDTO.Start) >= 0 && DateTime.Compare(h.Date, requestDTO.End) <= 0).ToList();
+                    filteredHistory.Sort();
+                    int count = 0;
+                    while (true)
+                    {
+                        if (count % requestDTO.Frequency == 0)
+                        {
+                            DateTime date = requestDTO.Start.AddDays(count);
+                            if (DateTime.Compare(date, filteredHistory[^1].Date) > 0) break;
+                            //Get the closet available trading day with price
+                            foreach (var item in filteredHistory)
+                            {
+                                if (DateTime.Compare(item.Date, date) >= 0)
+                                {
+                                    result += item.Date.ToString() + "\n";
+                                    break;
+                                }
+                            }
+                        }
+                        count++;
+                    }
+                }
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return "failed!";
+            }
+        }
+    }
+}
---
+++ DTOs/RequestDTO.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KFinance.DTOs
+{
+    public class RequestDTO
+    {
+        [Required]
+        public string? TickerSymbol { get; set; }
+        [Required]
+        public DateTime Start { get; set; }
+        [Required]
+        public DateTime End { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public int Frequency { get; set; }
+    }
+
+    public class ResponseFromAPI
+    {
+        public string Symbol { get; set; }
+        public List<History> Historical { get; set; }
+    }
+
+    public class History : IComparable<History>
+    {
+        public DateTime Date { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public decimal AdjClose { get; set; }
+        public long Volume { get; set; }
+        public double UnadjustedVolume { get; set; }
+        public decimal Change { get; set; }
+        public decimal ChangePercent { get; set; }
+        public decimal Vwap { get; set; }
+        public string Label { get; set; }
+        public decimal ChangeOverTime { get; set; }
+
+        public int CompareTo(History? other)
+        {
+            if (other is null) return 1;
+
+            return DateTime.Compare(Date, other.Date);
+        }
+    }
+}
